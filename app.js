2b70f2c60724b7971ead1e45fb1e46e1b8abe634
top80276 <108623955+top80276@users.noreply.github.com>
2022-07-15 13:55:44 +0800

    222

--- app.js
+++ app.js
@@ -13,26 +13,6 @@ var emprouter = require('./router/emprouter');
 app.set('view engine','ejs');
 app.use('/employee',emprouter);
 
-// app.get('/', (req, res) => {
-//     res.sendFile('app.html');
-// });
-
-const server = http.createServer((request, response) => {
-    response.writeHead(200, {
-        'Content-Type': 'text/html'
-    });
-    fs.readFile('./index.html', null, function (error, data) {
-        if (error) {
-            response.writeHead(404);
-            respone.write('Whoops! File not found!');
-        } else {
-            response.write(data);
-        }
-        response.end();
-    });
-});
-
-server.listen(port, () => {
-    console.log(`Server is listening on port number: ${port}`);
-
+app.get('/', (req, res) => {
+    fs.res.sendFile('app.html');
 });
