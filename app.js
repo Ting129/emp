8bff876f9cb46b72c1f22d7fe9126b9714dd9cee
top80276 <108623955+top80276@users.noreply.github.com>
2022-07-15 13:46:00 +0800

    111

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+let fs = require('fs');
 // app.listen(8181);
 
 // Establishing the port
@@ -12,6 +13,26 @@ var emprouter = require('./router/emprouter');
 app.set('view engine','ejs');
 app.use('/employee',emprouter);
 
-app.get('/', (req, res) => {
-    res.sendFile('app.html');
+// app.get('/', (req, res) => {
+//     res.sendFile('app.html');
+// });
+
+const server = http.createServer((request, response) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html'
+    });
+    fs.readFile('./app.html', null, function (error, data) {
+        if (error) {
+            response.writeHead(404);
+            respone.write('Whoops! File not found!');
+        } else {
+            response.write(data);
+        }
+        response.end();
+    });
+});
+
+server.listen(port, () => {
+    console.log(`Server is listening on port number: ${port}`);
+
 });
