0ebbf8b8c793fe18b965f25188c00ecdeb355d46
top80276 <108623955+top80276@users.noreply.github.com>
2022-07-15 16:08:33 +0800

    FIN

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const http = require('http');
 // app.listen(8181);
 
 // Establishing the port
-const PORT = process.env.PORT || 8181 || 8000 || 5000 || 5050 || 5500;
+const PORT = process.env.PORT || 8181 ;
 app.listen(PORT, console.log(
     `Server started on port ${PORT}`));
   
@@ -18,3 +18,33 @@ app.use('/employee',emprouter);
 //     fs.res.sendFile('./index.html');
 // });
 
+
+
+var mysql = require('mysql');
+var mysql_config = {
+    host:'us-cdbr-east-06.cleardb.net',
+    user:'b6cb1d44dee95a',
+    password:'6d956c57',
+    database:'heroku_12bcf914abfa859'
+};
+
+
+function disconnect_handler() {
+   let conn = mysql.createConnection(mysql_config);
+    conn.connect(err => {
+        (err) && setTimeout('disconnect_handler()', 2000);
+    });
+
+    conn.on('error', err => {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            // db error 重新連線
+            disconnect_handler();
+        } else {
+            throw err;
+        }
+    });
+    exports.conn = conn;
+}
+disconnect_handler();
+
+exports.disconnect_handler =  disconnect_handler;
